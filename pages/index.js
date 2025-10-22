335075247d5f59923195a5127f31605cf439a535
Lucas L. Mikuri <19478652+lucasmikuri@users.noreply.github.com>
2025-10-21 21:50:25 -0400

    Delete pages/clone-tabnews.code-workspace

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>Teste 5</div>;
+  return (
+    <div>
+      Se você me ama, manda uma declaração pelo whats
+      <a href="https://wa.me/5565999328503" target="_blank">
+        <br></br>Abrir e me mandar
+      </a>
+    </div>
+  );
 }
 
 export default Home;
